0682da25db466ba4c190f2af72468328c2e8743a
miguel acosta <miguelacosta90@gmail.com>
2019-02-27 12:51:46 -0800

    login and out flutter app

---
+++ lib/ui/Login.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new LoginState();
+  }
+}
+
+class LoginState extends State<Login> {
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
+  String _welcomeString = "";
+
+  void _erase() {
+    setState(() {
+      _userController.clear();
+      _passwordController.clear();
+    });
+  }
+
+  void _showWelcome() {
+    setState(() {
+      if (_userController.text.isNotEmpty && _passwordController.text.isNotEmpty) {
+        _welcomeString = _userController.text;
+      } else
+        _welcomeString = "Nothing";
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Login"),
+        centerTitle: true,
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: <Widget>[
+            new Image.asset(
+              'img/whaleIcon.png',
+              width: 100.0,
+              height: 100.0,
+              color: Colors.lightBlueAccent,
+            ),
+            //form
+            new Container(
+              height: 200.0,
+              width: 420.0,
+              color: Colors.white,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: _userController,
+                    decoration: new InputDecoration(
+                        hintText: 'User Name', icon: new Icon(Icons.person)),
+                  ),
+                  new TextField(
+                    controller: _passwordController,
+                    decoration: new InputDecoration(
+                        hintText: 'Password', icon: new Icon(Icons.lock)),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(10.5)),
+                  new Center(
+                    child: new Row(
+                      children: <Widget>[
+                        //Login
+                        new Container(
+                          margin: const EdgeInsets.only(left: 59.0),
+                          child: new RaisedButton(
+                            onPressed: _showWelcome,
+                            child: const Text('Login'),
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                        //Logout
+                        new Container(
+                          margin: const EdgeInsets.only(left: 120.0),
+                          child: new RaisedButton(
+                            onPressed: _erase,
+                            child: const Text('Logout'),
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ), //form
+            new Padding(padding: new EdgeInsets.all(10.5)),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  "welcome, $_welcomeString",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
